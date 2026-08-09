a5e29ebb0e2df62ae77f6fb8e014b264f800cb97
AncientSun <70993316+Isle4525@users.noreply.github.com>
2026-08-10 01:25:30 +0500

    chore: init project

---
+++ src/main/java/com/example/ai_dev_assistent/AiDevAssistentApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_dev_assistent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDevAssistentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiDevAssistentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_dev_assistent/AiDevAssistentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_dev_assistent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiDevAssistentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
