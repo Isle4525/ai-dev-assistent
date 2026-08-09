c302e59d5a603b0bdbefb3f1b5b3cc1fc6aa7180
AncientSun <70993316+Isle4525@users.noreply.github.com>
2026-08-10 01:38:24 +0500

    feat: add constructors, getter/setters in model

--- src/main/java/com/example/ai_dev_assistent/domain/GitHubRepository.java
+++ src/main/java/com/example/ai_dev_assistent/domain/GitHubRepository.java
@@ -2,19 +2,24 @@ package com.example.ai_dev_assistent.domain;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "github_repository")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GitHubRepository {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String owner;
-
     private String name;
-
     private String url;
 
 
