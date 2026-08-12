615004dfd0e938bf9a0b2e05c10044399b02835b
AncientSun <70993316+Isle4525@users.noreply.github.com>
2026-08-12 05:20:04 +0500

    chore: add in app properties logger properties and db connection

--- src/main/java/com/example/ai_dev_assistent/service/GitHubRepositoryService.java
+++ src/main/java/com/example/ai_dev_assistent/service/GitHubRepositoryService.java
@@ -4,14 +4,19 @@ import com.example.ai_dev_assistent.domain.GitHubRepository;
 import com.example.ai_dev_assistent.dto.Request;
 import com.example.ai_dev_assistent.dto.Response;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
 import org.springframework.stereotype.Service;
 
+import static com.example.ai_dev_assistent.service.GitHubRepoInfoParser.extractOwnerAndRepo;
+
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class GitHubRepositoryService {
 
     private final GitHubRepositoryService gitHubRepositoryService;
-
 
 
 
