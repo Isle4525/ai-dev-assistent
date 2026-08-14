664bafe9828b6da774f1c63e5215c8e65fc4b20c
AncientSun <70993316+Isle4525@users.noreply.github.com>
2026-08-14 09:30:48 +0500

    fix: change extends in repository

--- src/main/java/com/example/ai_dev_assistent/repository/GitHubRepositoryRepository.java
+++ src/main/java/com/example/ai_dev_assistent/repository/GitHubRepositoryRepository.java
@@ -1,9 +1,10 @@
 package com.example.ai_dev_assistent.repository;
 
 import com.example.ai_dev_assistent.domain.GitHubRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GitHubRepositoryRepository extends CrudRepository<GitHubRepository, Long> {
+public interface GitHubRepositoryRepository extends JpaRepository<GitHubRepository, Long> {
 }
